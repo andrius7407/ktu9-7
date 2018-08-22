10a49909c2e52ef8f7ee8b450a0f3cba9798f872
andrius7407 <andrius.el@gmail.com>
2018-08-22 12:32:52 +0300

    Add project files.

---
+++ ktu9-7/Rezultatai.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ktu9_7
+{
+    class Rezultatai
+    {
+        public int skaiciusSuma;
+        public int skaitiklisSuma;
+        public int vardiklisSuma;
+        public int skaiciusSkirt;
+        public int skaitiklisSkirt;
+        public int vardiklisSkirt;
+
+        public Rezultatai(int skaiciusSuma, int skaitiklisSuma, int vardiklisSuma, int skaiciusSkirt, int skaitiklisSkirt, int vardiklisSkirt)
+        {
+            this.skaiciusSuma = skaiciusSuma;
+            this.skaitiklisSuma = skaitiklisSuma;
+            this.vardiklisSuma = vardiklisSuma;
+            this.skaiciusSkirt = skaiciusSkirt;
+            this.skaitiklisSkirt = skaitiklisSkirt;
+            this.vardiklisSkirt = vardiklisSkirt;
+        }
+    }
+}
